fdcb49190e93f9c9716e644be1c9accbc0bd3d38
Beris Dizdar <Berisd2010@gmail.com>
2023-07-21 13:04:05 -0400

    Data dictionary final version

--- Data_Dictionary.py
+++ Data_Dictionary.py
@@ -2,7 +2,7 @@
 
 import pandas as pd 
 
-Data_Dictionary = {'ID':['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'],
+Data_Dictionary = {'ID':['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17'],
                    'Variable':['Metro_Pop_2020_DF1', 'CBSA', 'City_Metro', 'LSAD', 'Date', 'Population', 'Metro_Pop_2000_DF2', 'Population_2000',
                                'Population_2010', 'GDP_Metro_DF3', 'Description', 'LineCode', 'Merged_Data', 'PopulationDiff', 'DF1_MetPop2020', 'DF2_Metro_Pop2000', 'DF3_Metro_GDP'],
                    'Data_Type':['DataFrame', 'int64', 'object', 'object', 'int64', 'int64', 'DataFrame', 'int64', 'int64', 'DataFrame', 'object', 'int64',
